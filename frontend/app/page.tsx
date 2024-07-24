044f8aa3014137f9bc45f80bbbb46bae0878277b
Zakaria Wina <winazakaria0@gmail.com>
2024-07-24 17:15:48 +0100

    SETUP: Next.js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="font-mono text-9xl text-center">Welcome to Zwizzer!</h1>
+    </div>
+  );
+}
