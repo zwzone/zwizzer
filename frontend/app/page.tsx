63381c83d9ed7bb47433870fd6dd9afa0a583cd1
Zakaria Wina <winazakaria0@gmail.com>
2024-07-24 18:42:01 +0100

    SETUP: shadcn/ui

--- frontend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex-center h-screen">
-      <h1 className="font-mono text-9xl text-center">Welcome to Zwizzer!</h1>
-    </div>
-  );
-}
