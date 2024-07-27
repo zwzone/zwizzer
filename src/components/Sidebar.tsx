dca150bf5bef377b40d664f84901233963411dfe
Achraf-ahrach <achrafahrach44@gmail.com>
2024-07-27 20:18:53 +0100

    ADD: Sidebar responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@components/ui/button";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Avatar, AvatarImage } from "@src/components/ui/avatar";
-import { Ellipsis, House, Icon } from "lucide-react";
+import { Bookmark, Ellipsis, House, Settings } from "lucide-react";
 import { UserRound } from "lucide-react";
 import { PenTool } from "lucide-react";
 import Link from "next/link";
@@ -16,32 +16,38 @@ const NAVIGATION_BAR = [
     icon: <UserRound />,
   },
   {
-    title: "Post",
-    icon: <PenTool />,
+    title: "Bookmarks",
+    icon: <Bookmark />,
   },
+  {
+    title: "Settings",
+    icon: <Settings />,
+  },
+  {
+    title: "Post",
+    icon: <PenTool className="lg:hidden"/>,
+  }
 ];
 
 export default function Sidebar() {
   return (
-    <section className="flex-between h-full flex-col px-2 py-4 lg:w-64">
-      <div className="flex-center flex-col gap-6">
+    <section className="flex justify-between h-full flex-col px-2 py-4">
+      <div className="flex items-center lg:items-start flex-col gap-2">
         {NAVIGATION_BAR.map((item) => (
-          <Link key={item.title} href={"/" + item.title.toLowerCase()}>
+          <Link className={`${item.title === "Post" && "lg:self-stretch"}`} key={item.title} href={"/" + item.title.toLowerCase()}>
             <Button
-              variant={item.title !== "Post" ? "outline" : "default"}
-              className="flex-center size-14 gap-1 rounded-full p-0"
+              variant={item.title !== "Post" ? "ghost" : "default"}
+              className="flex-center gap-4 rounded-full px-2 lg:pr-6 w-full"
             >
               {item.icon}
-              {item.title !== "Post" && (
-                <span className="hidden font-bold lg:inline">{item.title}</span>
-              )}
+              <span className="hidden font-bold lg:inline">{item.title}</span>
             </Button>
           </Link>
         ))}
       </div>
       <Button
-        variant="outline"
-        className="flex h-14 items-center gap-2 rounded-full border-none p-2"
+        variant="ghost"
+        className="flex h-14 items-center gap-2 rounded-full p-2"
       >
         <Avatar className="size-10 justify-self-end">
           <AvatarImage
