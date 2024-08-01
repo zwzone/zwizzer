6841413b2f1fa70a605994dd07203bab434953de
Achraf-ahrach <achrafahrach44@gmail.com>
2024-08-01 16:45:31 +0100

    ADD: Completing Zweez component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,40 @@ import Sidebar from "@src/components/Sidebar";
 import Textpost from "@src/components/Textpost";
 import { Avatar, AvatarFallback, AvatarImage } from "@src/components/ui/avatar";
 import { Button } from "@src/components/ui/button";
+import Zweez from "@components/Zweez";
+
+const Zweezs = [
+  {
+    id: 1,
+    avatar: "https://github.com/shadcn.png",
+    fullname: "Zakaria Wina",
+    username: "@zwzone",
+    content: "Hello, World!",
+    replies: 1234,
+    reposts: 1324,
+    likes: 445,
+  },
+  {
+    id: 2,
+    avatar: "https://github.com/shadcn.png",
+    fullname: "Achraf Ahrach",
+    username: "@aachraf",
+    content: "Hello, World Again!",
+    replies: 1234,
+    reposts: 1324,
+    likes: 445,
+  },
+  {
+    id: 3,
+    avatar: "https://github.com/shadcn.png",
+    fullname: "Ayoub Ayoub",
+    username: "@AAYY",
+    content: "What is happening here guys?",
+    replies: 1234,
+    reposts: 1324,
+    likes: 445,
+  },
+];
 
 export default function Home() {
   return (
@@ -13,22 +47,23 @@ export default function Home() {
         <section className="min-h-full border-x">
           {/* Post Form */}
           <div className="flex flex-col gap-4 border-b p-4 sm:flex-row sm:items-start sm:px-8">
-            <div>
-              <Avatar className="size-10 justify-self-end">
-                <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
-                <AvatarFallback>ZW</AvatarFallback>
-              </Avatar>
-            </div>
+            <Avatar className="size-12 justify-self-end">
+              <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
+              <AvatarFallback>ZW</AvatarFallback>
+            </Avatar>
             {/* Post Input */}
             <div className="flex flex-grow flex-col gap-8">
               <Textpost />
-              <Button type="submit" className="self-end rounded-full">
+              <Button type="submit" className="sm:self-end rounded-full px-8">
                 Post
               </Button>
             </div>
           </div>
+          {/* TODO: Add Zweez components here */}
+          {Zweezs.map((zweez) => (
+            <Zweez key={zweez.id} {...zweez} />
+          ))}
         </section>
-        {/* TODO: Add Zweez components here */}
       </main>
     </div>
   );
--- src/components/Zweez.tsx
+++ src/components/Zweez.tsx
@@ -1,11 +1,78 @@
-export default function Zweez() {
-  /* TODO:
-  - Your work must be done at `add-home-page` branch.
-  - Your task is to make a small component of Zwizzer's posts that are similar to X platform (Twitter)
-  - Name this component "Zweez" ( same as Tweet in Twitter )
-  - The first features of Zweez compoents are:
-      - { avatar + name + username } of the poster
-      - The content of the post ( which will be just text for the moment )
-      - Replies + Likes */
-  return <div>Zweez</div>;
+import { Avatar, AvatarFallback, AvatarImage } from "@components/ui/avatar";
+import {
+  Ellipsis,
+  MessageCircle,
+  Repeat2,
+  Heart,
+  Bookmark,
+  Share,
+} from "lucide-react";
+import { Button } from "@components/ui/button";
+
+export default function Zweez({
+  avatar,
+  fullname,
+  username,
+  content,
+  replies,
+  reposts,
+  likes,
+}: {
+  avatar: string;
+  fullname: string;
+  username: string;
+  content: string;
+  replies: number;
+  reposts: number;
+  likes: number;
+}) {
+  return (
+    <div className="relative flex flex-col gap-4 border-b p-4 sm:flex-row sm:items-start sm:px-8">
+      <Button
+        variant="ghost"
+        className="absolute right-2 top-4 rounded-full px-2"
+      >
+        <Ellipsis className="size-6" />
+      </Button>
+      <Avatar className="size-8 justify-self-end">
+        <AvatarImage src={avatar} alt="avatar" />
+        <AvatarFallback>ZW</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-grow flex-col gap-4">
+        <div className="flex flex-wrap gap-x-2">
+          <span className="font-bold leading-none">{fullname}</span>
+          <span className="leading-none text-gray-500">{username}</span>
+        </div>
+        <div>{content}</div>
+        <div className="flex-between">
+          <div className="flex-center gap-1">
+            <Button variant="ghost" className="rounded-full px-2">
+              <MessageCircle />
+            </Button>
+            {replies}
+          </div>
+          <div className="flex-center gap-1">
+            <Button variant="ghost" className="rounded-full px-2">
+              <Repeat2 />
+            </Button>
+            {reposts}
+          </div>
+          <div className="flex-center gap-1">
+            <Button variant="ghost" className="rounded-full px-2">
+              <Heart />
+            </Button>
+            {likes}
+          </div>
+          <div className="flex gap-3">
+            <Button variant="ghost" className="rounded-full px-2">
+              <Bookmark />
+            </Button>
+            <Button variant="ghost" className="rounded-full px-2">
+              <Share />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
